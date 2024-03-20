8b191c4861afb80e8ff0bee3ea7891bac4d76035
Nikolai Lipski <>
2024-03-20 16:50:17 -0300

    start of the project

---
+++ TestWeather/TestWeather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestWeather
+//
+//  Created by Nikolai Lipski on 20.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestWeather/TestWeather/TestWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestWeatherApp.swift
+//  TestWeather
+//
+//  Created by Nikolai Lipski on 20.03.24.
+//
+
+import SwiftUI
+
+@main
+struct TestWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
